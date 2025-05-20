d5c58c49a3fc2ed7ea0048eef5835d10000f34ed
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-20 16:44:55 +0500

    Commited

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 import random
+import uuid
 from datetime import datetime, timedelta
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from shared.models import BaseModel
 
@@ -52,6 +54,44 @@ class User(AbstractUser, BaseModel):
             code=code
         )
         return code
+
+    def check_username(self):
+        if not self.username:
+            temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
+            while User.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0, 9)}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split('-')[1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_shak256'):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh": str(refresh)
+        }
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(User, self).save(*args, **kwargs)
+
+    def clean(self):
+        self.check_email()
+        self.check_username()
+        self.check_pass()
+        self.hashing_password()
 
 class UserConfirmation(BaseModel):
     TYPE_CHOICES = (
