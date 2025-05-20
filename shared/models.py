f2c9ec24cdf0639ead3c97b5203c27754da8f7d9
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-20 15:02:53 +0500

    Commited

--- shared/models.py
+++ shared/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        pass
+        abstract = True
--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from shared.models import BaseModel
 
 ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", "manager", "admin")
 VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
 NEW, CODE_VERIFIED, DONE, PHOTO_STEP = ("new", "code_verified", "done", "photo_step")
 
 
-class User(AbstractUser):
+class User(AbstractUser, BaseModel):
     USER_ROLES = (
         (ORDINARY_USER, ORDINARY_USER),
         (MANAGER, MANAGER),
@@ -30,4 +31,7 @@ class User(AbstractUser):
     email = models.EmailField(null=True, blank=True, unique=True)
     phone_number = models.CharField(max_length=13, blank=True, null=True, unique=True)
     photo = models.ImageField(upload_to="user_photos", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
