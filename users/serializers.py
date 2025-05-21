52fbcda3735dbe5cc2fb38df128ece541ecebf85
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-21 16:14:29 +0500

    Commited

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,9 @@
+from xml.dom import ValidationErr
+
 from rest_framework import serializers
 
 from shared.utility import check_email_or_phone
-from users.models import User
+from users.models import User, VIA_EMAIL, VIA_PHONE
 
 
 class SignUpSerializer(serializers.ModelSerializer):
@@ -23,6 +25,16 @@ class SignUpSerializer(serializers.ModelSerializer):
             'auth_status': {'read_only': True, 'required': False}
         }
 
+    def create(self, validated_data):
+        user = super(SignUpSerializer, self).create(validated_data)
+
+        if user.auth_type == VIA_EMAIL:
+            code = user.create_verify_code(verify_type=VIA_EMAIL)
+
+        elif user.auth_type == VIA_PHONE:
+            code = user.create_verify_code(verify_type=VIA_PHONE)
+        user.save()
+
     def validate(self, data):
         super(SignUpSerializer, self).validate(data)
         data = self.auth_validate(data)
@@ -32,5 +44,26 @@ class SignUpSerializer(serializers.ModelSerializer):
     def auth_validate(data):
         user_input = str(data.get('email_phone_number')).lower()
         input_type = check_email_or_phone(email_or_phone=user_input)
-        print("user_input", user_input)
-        print("input_type", input_type)
+
+        if input_type == "email":
+            data = {
+                "email": input_type,
+                "auth_type": VIA_EMAIL
+            }
+        elif input_type == "phone":
+            data = {
+                "phone_number": input_type,
+                "auth_type": VIA_PHONE
+            }
+        else:
+            data = {
+                "success": False,
+                "message": "Elektron manzil yoki telefon raqam kiritilishi lozim!"
+            }
+            raise ValidationErr(data)
+        print("data", data)
+        return data
+
+    def validate_email_phone_number(self, value):
+        value = value.lower()
+        return value
