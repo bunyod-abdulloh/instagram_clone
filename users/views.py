639360e0fe3b8180c5b1351e527ff8a76fc8b9d4
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-24 12:32:32 +0500

    Commited

--- users/views.py
+++ users/views.py
@@ -7,8 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
-from users.models import User, NEW, CODE_VERIFIED
+from shared.utility import send_email
+from users.models import User, NEW, CODE_VERIFIED, VIA_EMAIL, VIA_PHONE
 from users.serializers import SignUpSerializer
 
 
@@ -38,7 +38,7 @@ class VerifyAPIView(APIView):
     @staticmethod
     def check_verify(user, code):       # 12:03 -> 12:05 => expiration_time=12:05   12:04
         verifies = user.verify_codes.filter(expiration_time__gte=datetime.now(), code=code, is_confirmed=False)
-        print(verifies)
+
         if not verifies.exists():
             data = {
                 "message": "Tasdiqlash kodingiz xato yoki eskirgan"
@@ -50,3 +50,38 @@ class VerifyAPIView(APIView):
             user.auth_status = CODE_VERIFIED
             user.save()
         return True
+
+
+class GetNewVerificationView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        self.check_verification(user)
+        if user.auth_type == VIA_EMAIL:
+            code = user.create_verify_code(VIA_EMAIL)
+            send_email(user.email, code)
+        elif user.auth_type == VIA_PHONE:
+            code = user.create_verify_code(VIA_PHONE)
+            send_email(user.phone_number, code)
+        else:
+            data = {
+                "message": "Email yoki telefon raqami notogri"
+            }
+            raise ValidationError(data)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Tasdiqlash kodingiz qaytadan jo'natildi."
+            }
+        )
+
+    @staticmethod
+    def check_verification(user):
+        verifies = user.verify_codes.filter(expiration_time__gte=datetime.now(), is_confirmed=False)
+        if verifies.exists():
+            data = {
+                "message": "Kodingiz hali ishlatish uchun yaroqli. Biroz kutib turing"
+            }
+            raise ValidationError(data)
