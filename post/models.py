7b5b81756241bdaf6e68ca66e954e57a79c4ca20
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-05-28 18:10:06 +0500

    Post app modellari yozildi!

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import FileExtensionValidator
+from django.core.validators import FileExtensionValidator, MaxLengthValidator
 from django.db import models
+from django.db.models import UniqueConstraint
 
 from shared.models import BaseModel
 
@@ -11,4 +12,46 @@ class Post(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     image = models.ImageField(upload_to='post_images', validators=[
         FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
-    caption = models.CharField()
+    caption = models.TextField(validators=[MaxLengthValidator(2000)])
+
+    class Meta:
+        db_table = "posts"
+        verbose_name = "post"
+        verbose_name_plural = "posts"
+
+
+class PostComment(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField(validators=[MaxLengthValidator(2000)])
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )  # Comment ichidagi commentga reply qilingan xabar uchun
+
+
+class PostLike(BaseModel):
+    # postlarga likelar bosilganini ko'rish mumkin
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'post'],
+            )
+        ]
+
+
+class CommentLike(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(PostComment, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'comment'],
+            )
+        ]
